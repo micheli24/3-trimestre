20c31df3bed2da91575b9fee2a3f972e5c72bc11
micheli24 <162988719+micheli24@users.noreply.github.com>
2024-11-18 08:56:47 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Geografia',
+    'qual a capital do México?',
+    'A capital do México é Cidade do México'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geografia',
+    'qual a capital da Romenia?',
+    'A capital da Romenia é Bucareste'
 )
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Geografia',
+    'Qual a capital Italia?',
+    'A capital da Italía é Roma'
 ) 
